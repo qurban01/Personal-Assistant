07454af26e5286480d4c0a4855a4f57c79d89fd4
qurban01 <135748538+qurban01@users.noreply.github.com>
2026-08-16 18:18:27 +0500

    Update index.js

--- index.js
+++ index.js
@@ -384,5 +384,4 @@ mongoose.connect(process.env.MONGO_URI)
     })
     .catch(err => {
         console.log('MongoDB connection error:', err.message);
-    });MongoDB connection error:', err.message);
     });
